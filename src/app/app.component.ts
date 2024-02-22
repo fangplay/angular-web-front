a2d5329655465aba72c29ad76ab84d4482cd9fd9
fangplay <fangplay_2014@hotmail.com>
2024-02-22 14:23:36 +0700

    alpha1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `<h1>Hello world!</h1>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-web-front';
+  title = 'AngularWebFront - Alpha Release';
 }
